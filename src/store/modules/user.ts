24140756e3231aa35ce8b5af45ecfa4b72583ddc
dison <dison0509@gmail.com>
2022-07-15 18:38:39 +0800

    fix: prettier.config.js

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,21 +1,8 @@
-import { loginPassword } from '/@/api';
-import { useCookies } from '@vueuse/integrations/useCookies';
 import { defineStore } from 'pinia';
-import { watch } from 'vue';
-import { AnyObject } from '/#/global';
-
-const { VITE_TOKEN_KEY } = import.meta.env;
-const token = useCookies().get(VITE_TOKEN_KEY as string);
-
-interface StoreUser {
-  token: string;
-  info: AnyObject;
-}
 
 export const useUserStore = defineStore({
   id: 'app-user',
-  state: (): StoreUser => ({
-    token: token,
+  state: () => ({
     info: {},
   }),
   getters: {
@@ -25,17 +12,10 @@ export const useUserStore = defineStore({
   },
   actions: {
     setInfo(info: any) {
-      this.info = info ? info : '';
+      console.log(info);
     },
     login() {
-      return new Promise((resolve) => {
-        const { data } = loginPassword();
-        watch(data, () => {
-          this.setInfo(data.value);
-          // useCookies().set(VITE_TOKEN_KEY as string, data.value.token);
-          resolve(data.value);
-        });
-      });
+      console.log('login');
     },
   },
 });
